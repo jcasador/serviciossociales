fa778c78d1f1266ee10530960aef26e0241a63bc
jcasador <jcasador@minsait.com>
2021-11-26 18:01:18 +0100

    cambios en config

--- wp-config.php
+++ wp-config.php
@@ -20,27 +20,24 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'junta-sersoc');
+define( 'DB_NAME', '6902213_wp_mpl3y' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root');
+define( 'DB_USER', 'wp_qx674' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root');
+define( 'DB_PASSWORD', 'ppCi1eNO%1%5*eOq' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost');
+define( 'DB_HOST', 'PMYSQL113.dns-servicio.com:3306' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '');
+define( 'DB_COLLATE', '' );
 
-/** Automatic updater disabled (Core, Plugins and Themes) */
-define( 'AUTOMATIC_UPDATER_DISABLED', true );
-
-/**#@+
+/**
  * Authentication Unique Keys and Salts.
  *
  * Change these to different unique phrases!
@@ -49,16 +46,14 @@ define( 'AUTOMATIC_UPDATER_DISABLED', true );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '9158e926425ed74f2cabb5ef3be0e20b6553d167');
-define( 'SECURE_AUTH_KEY',  '9ac8729b043ae85adbe51a60daaae508846edb6b');
-define( 'LOGGED_IN_KEY',    'ae153a101d39ceb38164b2075d7328beea4cb45d');
-define( 'NONCE_KEY',        'dfa50f72ce8745b1765297660b61d0f37d8009c1');
-define( 'AUTH_SALT',        '5bc8505aa707135293c6ee864892a352fe67c10a');
-define( 'SECURE_AUTH_SALT', '3c41ccef1c8a7bccab9500350be905c5a4630225');
-define( 'LOGGED_IN_SALT',   'f59216f1d3336c3d81308707942f08216168423d');
-define( 'NONCE_SALT',       '042dbec372e70940660090e610ed90bf8728f620');
-
-/**#@-*/
+define('AUTH_KEY', 'fT4!n0/oO1y(/&qMo50R~v67Qz~:+:-#];e6R4r&;)x4-+&9~&l4Yg5ny]HIS0dS');
+define('SECURE_AUTH_KEY', 'l~vT%K8rVMw1mlBsX9*kKs9/Jm4r;B03W__+!;;[7;mG5/jzg33Z84PS;)+@_@2n');
+define('LOGGED_IN_KEY', '9r4f:Ol*q0-%gR41YA;;9f[nu#f0*8BkQ@P#;g0&+7vGRZ!7PcP_L1ccE66Z0qS)');
+define('NONCE_KEY', '4Tt1_yU9t2krX9o&x)S:]Oa]ex4V*96%UJ%k0b9k]22mD|:RbT!AXDBfM3H!E|2)');
+define('AUTH_SALT', 'k*5nm1VgQ1*893Aq%0[mg0Ho%6I-4Lk0Z31FwBi(%h4s;]w9_3n2o32|7IQ2Z1X#');
+define('SECURE_AUTH_SALT', '7OHP;%C_+!|v%Z/m!L!%&j91RwN4F3406DQMG1rszpQZa;l@J5W*/4ZNf+H1s6e5');
+define('LOGGED_IN_SALT', '(4:C(t;*Hj+JZD*[s#@B|QW@DA8d-PAsWnc/&1)!I8-2kf5/K3jd/Rv]z8706_3(');
+define('NONCE_SALT', 'b1S/:n8g6jx|M;VM2:(QCB1kI6!U(sIJUE:/*8uEw0~;Hvq948b(T9S[53&YoV-0');
 
 /**
  * WordPress Database Table prefix.
@@ -66,38 +61,15 @@ define( 'NONCE_SALT',       '042dbec372e70940660090e610ed90bf8728f620');
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wp_';
+$table_prefix = '4Wd7f3i3_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
-define( 'WP_DEBUG', false );
 
-/* Disable Editing in Dashboard */
-define('DISALLOW_FILE_EDIT', false);
-/* Desactiva cron */
-define('DISABLE_WP_CRON', true);
-
-// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
-
+define('WP_ALLOW_MULTISITE', true);
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
